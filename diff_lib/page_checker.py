645e4fa885004d1fd34c028a655456d6d93e655b
katsanis <yannis.katsanis@gmail.com>
2020-06-21 17:37:33 +0300

    THRESHOLD fix

--- diff_lib/page_checker.py
+++ diff_lib/page_checker.py
@@ -5,7 +5,7 @@ import download as dow
 import diff_checker as dc
 import requests, json, datetime
 
-THRESHOLD = 1
+THRESHOLD = 0.97
 INTERVAL = 1
 WEIGHT = 0.1
 API = 'http://localhost:8000/links/all/'
@@ -31,11 +31,11 @@ class page_checker:
     def api_call_for_contents(self, my_instance, all_instances):
         self.contents = json.loads(requests.get(API + '?page_checker_id={}&page_checkers={}'.format(my_instance, all_instances)).text)
 
-    def api_call_for_link_change(self, db_id, url, new_content, timer, penalty, has_changed, version):
+    def api_call_for_link_change(self, db_id, url, old_content, new_content, timer, penalty, has_changed, version):
         data = {
             'id' : db_id,
             'url' : url,
-            'content' : new_content,
+            'content' : new_content if has_changed else old_content,
             'next_check' : timer,
             'penalty' : penalty,
         }
@@ -75,7 +75,7 @@ class page_checker:
 
     def notify(self, db_id, has_changed, old_content, new_content, url, old_timer, penalty, version):
         timer, penalty = self.update_url_timer(has_changed, old_timer, penalty)
-        self.api_call_for_link_change(db_id, url, new_content, timer, penalty, has_changed, version)
+        self.api_call_for_link_change(db_id, url, old_content, new_content, timer, penalty, has_changed, version)
 
 
 if __name__ == "__main__":
